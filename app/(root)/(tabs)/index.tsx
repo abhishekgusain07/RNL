7e23faa1c2e715d52ebf026c4023c0b3fc3dcada
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-02-22 12:30:29 +0530

    custom fonts and colors

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
       }}
       className="bg-black text-white"
     >
-      <Text className="font-bold mb-3 text-white"> Testing Tailwindcss </Text>
+      <Text className="text-4xl font-rubik-extrabold mb-3 text-white"> Testing Tailwindcss </Text>
       <Link  href="/sign-in" className="text-white">Sign In</Link>
       <Link  href="/explore" className="text-white">Explore</Link>
       <Link  href="/profile" className="text-white">Profile</Link>
