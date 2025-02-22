bbbc240a59ece263fede71d49c3ff8f380ad9fa5
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-02-22 14:25:08 +0530

    bug: login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -21,5 +22,8 @@ export default function RootLayout() {
   if(!fontsLoaded){
     return null;
   }
-  return <Stack screenOptions={{headerShown: false}}/>;
+  return (
+    <GlobalProvider> 
+      <Stack screenOptions={{headerShown: false}}/>
+    </GlobalProvider>);
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
-
+import images from "@/constants/images"
+import icons from '@/constants/icons'
+import { login } from '@/lib/appWrite'
 const SignIn = () => {
+  const handleLogin = async() => { 
+    const result = await login();
+    if (result) {
+      console.log('Login success');
+    } else {
+      Alert.alert('Login failed');
+    }
+  }
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName="h-full">
+        <Image  source={images.onboarding} className='w-full h-4/6' resizeMode='contain'/>
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>
+            Welcome to ReState
+          </Text>
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+            Let's Get You Closer to {'\n'} Your <Text className='text-primary-300'>Dream Home</Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+            Login to Restate with Google
+          </Text>
+          <TouchableOpacity onPress={handleLogin}
+            className='bg-white shadow-md shadow-zinc-300 rounded-fullw-ful
+            py-4 mt-5'
+          >
+            <View className='flex flex-row items-center justify-center'>
+              <Image 
+                source={icons.google}
+                className='size-5'
+                resizeMode='contain'
+              />
+              <Text>Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default SignIn 
+export default SignIn ;
